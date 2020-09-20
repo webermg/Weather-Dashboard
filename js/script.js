75c2535c5dbf51f8f89e3a397dc9b1266178b067
webermg <matthew.gerald.weber@gmail.com>
2020-09-20 12:50:17 -0700

    added function to get results

--- js/script.js
+++ js/script.js
@@ -20,4 +20,18 @@ const init = () => {
     
 }
 
+const getResults = (city) => {
+    const api = '432919c539be1e9eaadf34617ce6b063';
+    queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response) {
+        console.log(response);
+    }).err(function(error) {
+        console.log(error);
+    })
+}
+
 init();
