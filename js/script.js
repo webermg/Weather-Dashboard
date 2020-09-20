f051887b257e379af416faebcf92343387c644e6
webermg <matthew.gerald.weber@gmail.com>
2020-09-20 16:25:26 -0700

    added function to load weather forecast

--- js/script.js
+++ js/script.js
@@ -17,6 +17,8 @@ const cityInput = $("#city");
 const todayResults = $("#result-today");
 const forecastCards = $("#forecast-cards .card");
 
+const MS_IN_DAY = 86400000;
+
 const init = () => {
     
 }
@@ -24,22 +26,26 @@ const init = () => {
 const getResults = (city) => {
     const api = '432919c539be1e9eaadf34617ce6b063';
     queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`
-    
+    let test = `https://geocode.xyz/${city}?json=1`
 
     $.ajax({
-        url: queryURL,
+        url: test,
         method: 'GET'
     }).done(function(response) {
+        //TODO remove this
         console.log(response);
-        const lat = response.coord.lat;
-        const lon = response.coord.lon;
+        //return;
+        const lat = response.latt;
+        const lon = response.longt;
         queryURL2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=${api}`;
         $.ajax({
             url: queryURL2,
             method: 'GET'
         }).done(function(response) {
+            //TODO remove this
             console.log(response);
             loadTodayResult(response);
+            loadForecastResult(response);
         }).fail(function(error) {
             console.log(error.responseText);
         });
@@ -51,15 +57,30 @@ const getResults = (city) => {
 
 const loadTodayResult = response => {
     //header
+    let today = new Date();
     const header = todayResults.children("#today-header");
     header.children("#city-name").text(cityInput.val());
-    header.children("#today-date").text(`(today's date)`);
+    header.children("#today-date").text(`(${today.toLocaleDateString()})`);
     header.children("#today-icon").text("icon");
 
-    todayResults.children("#today-temp").text(response.current.temp);
-    todayResults.children("#today-humidity").text(response.current.humidity);
-    todayResults.children("#today-wind").text(response.current.wind_speed);
-    todayResults.children("#today-uv").text(response.current.uvi);
+    todayResults.children("#today-temp").text(`Temperature: ${response.current.temp} °F`);
+    todayResults.children("#today-humidity").text(`Humidity: ${response.current.humidity}%`);
+    todayResults.children("#today-wind").text(`Wind Speed:  ${response.current.wind_speed} MPH`);
+    todayResults.children("#today-uv").text(`UV Index: ${response.current.uvi}`);
+}
+
+const loadForecastResult = (response) => {
+    let day = 0;
+    let tomorrow = new Date();
+    tomorrow.setTime(tomorrow.getTime() + MS_IN_DAY);
+    forecastCards.each(function() {
+        $(this).find(".card-title").text(tomorrow.toLocaleDateString());
+        $(this).find("#forecast-icon").text("NYI");
+        $(this).find("#forecast-temp").text(`Temp: ${response.daily[day].temp.day} °F`);
+        $(this).find("#forecast-humidity").text(`Humidity: ${response.daily[day].humidity}%`);
+        day++;
+        tomorrow.setTime(tomorrow.getTime() + MS_IN_DAY);
+    });    
 }
 
 searchForm.on("submit", function(e) {
