c0b5c98936cb2634e2826541f45783c6f182ffd5
webermg <matthew.gerald.weber@gmail.com>
2020-09-20 12:49:27 -0700

    added html element handles

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//input
+//today results
+//  name date icon
+//  temp
+//  humidity
+//  windspeed
+//  uv-index
+//cards
+//  date
+//  icon
+//  temp
+//  humidity
+
+//element references
+const cityInput = $("#city");
+const todayResults = $("#result-today");
+const forecastCards = $("#forecast-cards .card");
+
+const init = () => {
+    
+}
+
+init();
